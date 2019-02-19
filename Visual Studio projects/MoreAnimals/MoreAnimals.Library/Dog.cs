9614943c2e1ca3b79a4504c204e6234b70dfaaeb
Impstar <magesbe@gmail.com>
2019-02-19 14:11:37 -0600

    created interface for animal library

--- Visual Studio projects/MoreAnimals/MoreAnimals.Library/Dog.cs
+++ Visual Studio projects/MoreAnimals/MoreAnimals.Library/Dog.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace MoreAnimals.Library
 {
-    class Dog
+    public class Dog
     {
+
+        //normally we stick to simple properties when we don't need any special checks
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+
+        public void MakeNoisee()
+        {
+            Console.WriteLine("Woof!");
+        }
+
+        public void GoTo(string location)
+        {
+            Console.WriteLine($"Walking to {location}");
+        }
+
     }
 }
---
+++ Visual Studio projects/MoreAnimals/MoreAnimals.Library/IAnimal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreAnimals.Library
+{
+    public interface IAnimal
+    {
+        //in an interface, the emmbers must have the same access level
+        //  as the whole interface, so we don't not say it explicitly
+        void MakeNoise();
+        void GoTo(string location);
+    }
+}
