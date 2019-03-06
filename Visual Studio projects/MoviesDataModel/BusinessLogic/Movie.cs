1d08ac4c886904a86f039e69ac082c019a0077fa
Impstar <magesbe@gmail.com>
2019-03-06 16:58:32 -0600

    ASP.NET stuff

---
+++ Visual Studio projects/MoviesDataModel/BusinessLogic/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public DateTime DateReleased { get; set; }
+    }
+}
